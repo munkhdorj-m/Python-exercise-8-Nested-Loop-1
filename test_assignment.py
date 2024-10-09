dad86752a02eb42136feb0f4749d33884675516d
Munkhdorj-M <64317250+munkhdorj-m@users.noreply.github.com>
2024-10-09 23:54:32 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -6,29 +6,26 @@ def check_contains_loop(function):
     source = inspect.getsource(function)
     return 'for' in source or 'while' in source
 
-def capsys(self, capsys):
-    self.capsys = capsys
-
-def test1(self):
+def test_print_pattern_1(capsys):
     expected_output = "* * * \n" * 5 
     print_pattern_1(5)
-    captured = self.capsys.readouterr()
+    captured = capsys.readouterr()
     assert captured.out == expected_output
 
-def test2(self):
+def test_print_pattern_2(capsys):
     expected_output = "1 \n2 1 \n3 2 1 \n4 3 2 1 \n5 4 3 2 1 \n" 
     print_pattern_2(5)
-    captured = self.capsys.readouterr()
+    captured = capsys.readouterr()
     assert captured.out == expected_output
 
-def test3(self):
+def test_print_pattern_3(capsys):
     expected_output = "1 \n2 3 \n4 5 6 \n7 8 9 10 \n11 12 13 14 15 \n" 
     print_pattern_3(5)
-    captured = self.capsys.readouterr()
+    captured = capsys.readouterr()
     assert captured.out == expected_output
 
-def test4(self):
-    expected_output = "    *\n   **\n  ***\n ****\n*****\n" 
+def test_print_pattern_4(capsys):
+    expected_output = "    *\n   **\n  ***\n ****\n*****\n"
     print_pattern_4(5)
-    captured = self.capsys.readouterr()
+    captured = capsys.readouterr()
     assert captured.out == expected_output
