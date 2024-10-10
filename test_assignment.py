12fff2c2d8952adb6c3a03f24fa9ee93594f5e28
Munkhdorj-M <64317250+munkhdorj-m@users.noreply.github.com>
2024-10-10 09:06:08 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,6 +1,6 @@
 import pytest
 import inspect
-from assignment import print_pattern_1, print_pattern_2, print_pattern_3, print_pattern_4
+from assignment import print_pattern_1, print_pattern_2, print_pattern_3
 
 def check_contains_loop(function):
     source = inspect.getsource(function)
@@ -14,18 +14,18 @@ def test1(capsys):
         "1 2 \n"
         "1 \n"
     )
-    print_pattern_2(5)
+    print_pattern_1(5)
     captured = capsys.readouterr()
     assert captured.out == expected_output
 
 def test2(capsys):
     expected_output = "1 \n2 3 \n4 5 6 \n7 8 9 10 \n11 12 13 14 15 \n" 
-    print_pattern_3(5)
+    print_pattern_2(5)
     captured = capsys.readouterr()
     assert captured.out == expected_output
 
 def test3(capsys):
     expected_output = "    *\n   **\n  ***\n ****\n*****\n"
-    print_pattern_4(5)
+    print_pattern_3(5)
     captured = capsys.readouterr()
     assert captured.out == expected_output
